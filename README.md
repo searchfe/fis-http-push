90f27b053f59ca2fe299fc5fec698de2bba47968
harttle <yangjvn@126.com>
2020-03-25 13:43:35 +0800

    feat: CLI usage fcp foo.txt http://example.com:8210/var/www/foo.txt, closes #2

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # fis-http-push
 
-FIS http push API, 用于 push 文件到 fis secure receiver（新版的 fis http-push）。
+FIS HTTP Push SDK, 用于 push 文件到 Fis Secure Receiver（新版的 fis http-push）。
 
 ## 编程接口
 
 demo/ 下有完整的例子。
 
+```javascript
+const {push} = require('fis-http-push');
+push('./main.js', '/var/www/main.js', {receiver: 'http://example.com:8210'})
 ```
 
+## 命令行接口
+
+```bash
+npm i -g fis-http-push
+fcp ./main.js http://example.com:8210/var/www/main.js
 ```
---
+++ src/bin/fcp.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import {resolve} from 'path';
+import * as yargs from 'yargs';
+import {push} from '../index';
+import {parseTargetUrl} from '../util';
+
+type OptionValue = string | undefined;
+
+yargs
+    .usage('$0 <SOURCE> <TARGET>')
+    .check(argv => {
+        if (argv._.length !== 2) {
+            throw new Error('source and target files must be specified');
+        }
+        return true;
+    });
+
+const source = resolve(yargs.argv._[0]);
+const {receiver, path} = parseTargetUrl(yargs.argv._[1]);
+push(source, path, {receiver});
